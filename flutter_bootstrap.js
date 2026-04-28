865bd58899a75f9e6b2fbd6b38e3f483ca450372
wine2siri <wine2siri@users.noreply.github.com>
2026-04-28 00:55:25 +0000

    deploy: 09f5d854f8711ca1daa438836330c93396b2e066

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3534304288"
+    serviceWorkerVersion: "3781864438"
   }
 });
